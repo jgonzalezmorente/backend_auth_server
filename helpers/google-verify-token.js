8d836009850bbc99531ff892c496813a2be1695c
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-03-30 17:33:01 +0200

    secret.json

--- helpers/google-verify-token.js
+++ helpers/google-verify-token.js
@@ -1,6 +1,6 @@
 const { OAuth2Client } = require('google-auth-library');
 
-const CLIENT_ID = '626541427885-9ol8e2k3gl280au5u52g79k62m0kr0i9.apps.googleusercontent.com';
+const CLIENT_ID = '';
 
 const client = new OAuth2Client(CLIENT_ID);
 
@@ -11,7 +11,7 @@ const validarGoogleIdToken = async (token) => {
             idToken: token,
             audience: [
                 CLIENT_ID,
-                '626541427885-1cuss2r6re2m04dqpv3qtqlfc07cjt5u.apps.googleusercontent.com',
+                '',
             ],
         });
         const payload = ticket.getPayload();
