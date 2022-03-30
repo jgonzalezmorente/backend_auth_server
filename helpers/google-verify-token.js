c661b9dd592523904c7332a530e1c486f6235fdc
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-03-30 16:53:05 +0200

    Google Id Token listo

---
+++ helpers/google-verify-token.js
@@ -0,0 +1,38 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const CLIENT_ID = '626541427885-9ol8e2k3gl280au5u52g79k62m0kr0i9.apps.googleusercontent.com';
+
+const client = new OAuth2Client(CLIENT_ID);
+
+const validarGoogleIdToken = async (token) => {
+
+    try {
+        
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: [
+                CLIENT_ID,
+                '626541427885-1cuss2r6re2m04dqpv3qtqlfc07cjt5u.apps.googleusercontent.com',
+            ],
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+        console.log(payload);
+    
+        return {
+            name: payload['name'],
+            picture: payload['picture'],
+            email: payload['email']
+        }
+    } catch (error) {
+        console.log(error);
+        return null;        
+    }
+
+    
+}
+
+
+module.exports = {
+    validarGoogleIdToken
+}
